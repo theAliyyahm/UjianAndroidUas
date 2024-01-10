617cbd4792050d525718b4a1dd4c410f68d4e189
theAliyyahm <maftukhatulaliyyah@gmail.com>
2024-01-10 15:02:47 +0700

    uas

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -18,9 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText edNamaDepan = (EditText) findViewById(R.id.edNamaDepan);
-        EditText edNamaBelakang = (EditText) findViewById(R.id.edNamaBelakang);
-        Button btnSimpan = (Button) findViewById(R.id.btnSimpan);
+        EditText edNamaDepan = findViewById(R.id.edNamaDepan);
+        EditText edNamaBelakang = findViewById(R.id.edNamaBelakang);
+        EditText edUmur = findViewById(R.id.edUmur);
+        Button btnSimpan = findViewById(R.id.btnSimpan);
 
         ArrayList<String> daftar_nama = new ArrayList<>();
 
@@ -31,17 +32,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String isian_nama_depan = edNamaDepan.getText().toString();
                 String isian_nama_belakang = edNamaBelakang.getText().toString();
+                String isian_umur = edUmur.getText().toString();
 
-                if(isian_nama_depan.isEmpty()){
+                if (isian_nama_depan.isEmpty() || isian_umur.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
-                }else{
-                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
-                    daftar_nama.clear();
-                    daftar_nama.add(nama_lengkap);
-                    edNamaDepan.setText("");
-                    edNamaBelakang.setText("");
-                    intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
-                    startActivity(intent_list);
+                } else {
+                    int umur = Integer.parseInt(isian_umur);
+                    if (umur <= 0) {
+                        Toast.makeText(getApplicationContext(), "Umur harus lebih dari 0", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String status;
+                        if (umur < 10) {
+                            status = "Anak-anak";
+                        } else if (umur < 20) {
+                            status = "Remaja";
+                        } else if (umur < 40) {
+                            status = "Dewasa";
+                        } else {
+                            status = "Tua";
+                        }
+                        String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
+                        daftar_nama.clear();
+
+                        // Membuat perulangan sesuai dengan nilai umur
+                        for (int i = 1; i <= umur; i++) {
+                            daftar_nama.add(i + " " + isian_nama_depan + " " + isian_nama_belakang + "," + "status :" + " " + status);
+                        }
+
+                        edNamaDepan.setText("");
+                        edNamaBelakang.setText("");
+                        edUmur.setText("");
+                        intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
+                        startActivity(intent_list);
+                    }
                 }
             }
         });
